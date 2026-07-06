8d99ce1c7ab512bb5805a648517083fd9277d8ea
brittanyhulme <brittany@kidera.com.au>
2026-07-06 11:44:10 +0800

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import KideraToyQuiz from "./KideraToyQuiz.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <KideraToyQuiz />
-  </React.StrictMode>
-);
